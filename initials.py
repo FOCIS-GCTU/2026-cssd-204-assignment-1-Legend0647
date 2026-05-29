eaa3b295124e60c94a7c7e88a46cd463eba1fadb
Winner Kodjo Buamey <2425402875@live.gctu.edu.gh>
2026-05-29 14:33:23 +0000

    Add completed assignment

--- initials.py
+++ initials.py
@@ -1,7 +1,29 @@
+# File: initials.py
+# Description: A program to print out my initials
+# Assignment Number: 2
+#
+# Name: Winner Kodjo Buamey
+# STUDENT ID:  2425402875
+# Email: 22425402875@live.gctu.edu.gh
+# Grader: Augustus
+#
+# On my honor, Winner Kodjo Buamey, this programming assignment is my own work
+# and I have not provided this code to any other student.
 
 def main():
-  pass #code goes here
-
-
+    print()
+    print("...WKB")
+    print()
+    print("...WW........WW........KK........KK........BBBBBBBBBB.......")
+    print("...WW........WW........KK.......KK.........BB.......BB......")
+    print("...WW........WW........KK......KK..........BB........BB.....")
+    print("...WW........WW........KK.....KK...........BB.......BB......")
+    print("...WW........WW........KKKKKKKK............BBBBBBBBBB.......")
+    print("...WW...WW...WW........KKKKKKKK............BBBBBBBBBB.......")
+    print("...WW..W..W..WW........KK.....KK...........BB.......BB......")
+    print("...WW.W....W.WW........KK......KK..........BB........BB.....")
+    print("...WWW......WWW...**...KK.......KK....**...BB.......BB....**")
+    print("...WW........WW...**...KK........KK...**...BBBBBBBBBB.....**")
+    print()
 
 main()
